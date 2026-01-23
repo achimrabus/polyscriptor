0776e0a337c5cf12b943099bd663a49aed082e19
Achim Rabus <achim.rabus@slavistik.uni-freiburg.de>
2026-01-23 17:19:03 +0100

    Remove hardcoded paths from divideInTestTrainLst.py - use CLI argument

--- party/divideInTestTrainLst.py
+++ party/divideInTestTrainLst.py
@@ -1,49 +1,66 @@
+#!/usr/bin/env python3
+"""
+Split a Party dataset.arrow file into train/val sets (80/20 split).
+
+Usage:
+    python divideInTestTrainLst.py <dataset.arrow>
+
+Example:
+    python divideInTestTrainLst.py ./dataset.arrow
+
+Outputs:
+    train.arrow, val.arrow, train.lst, val.lst
+"""
+
+import sys
 import pyarrow as pa
-
-# print("Reading arrow file...")
-# mmap = pa.memory_map('C:/Users/dhlabadmin/Desktop/m-test/party/dataset.arrow')
-
-# with mmap as source:
-#     array = pa.ipc.open_file(source).read_all()
-
-# print(array[0])
-# print(array[1])
-# print(array.schema)
-
 import pyarrow.ipc as ipc
 import numpy as np
 
-dataset_path = 'C:/Users/dhlabadmin/Desktop/m-test/party/dataset.arrow'
-with pa.memory_map(dataset_path, 'rb') as source:
-    table = ipc.open_file(source).read_all()
 
-num_rows = table.num_rows
-ids = np.random.permutation(num_rows)
-split = int(0.8 * num_rows)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python divideInTestTrainLst.py <dataset.arrow>")
+        print("Example: python divideInTestTrainLst.py ./dataset.arrow")
+        sys.exit(1)
 
-train_ids = ids[:split]
-val_ids = ids[split:]
+    dataset_path = sys.argv[1]
+    print(f"Reading {dataset_path}...")
 
-train_table = table.take(train_ids)
-val_table = table.take(val_ids)
+    with pa.memory_map(dataset_path, 'rb') as source:
+        table = ipc.open_file(source).read_all()
 
-train_path = 'train.arrow'
-val_path = 'val.arrow'
+    num_rows = table.num_rows
+    print(f"Total rows: {num_rows}")
 
-with pa.OSFile(train_path, 'wb') as sink:
-    with ipc.new_file(sink, train_table.schema) as writer:
-        writer.write(train_table)
+    ids = np.random.permutation(num_rows)
+    split = int(0.8 * num_rows)
 
-with pa.OSFile(val_path, 'wb') as sink:
-    with ipc.new_file(sink, val_table.schema) as writer:
-        writer.write(val_table)
+    train_ids = ids[:split]
+    val_ids = ids[split:]
 
-with open('train.lst', 'w', encoding="utf-8") as f:
-    f.write(train_path + '\n')
+    train_table = table.take(train_ids)
+    val_table = table.take(val_ids)
 
-with open('val.lst', 'w', encoding="utf-8") as f:
-    f.write(val_path + '\n')
+    train_path = 'train.arrow'
+    val_path = 'val.arrow'
 
-print('Done')
+    with pa.OSFile(train_path, 'wb') as sink:
+        with ipc.new_file(sink, train_table.schema) as writer:
+            writer.write(train_table)
 
-# file = open('train.lst', encoding='utf8')
+    with pa.OSFile(val_path, 'wb') as sink:
+        with ipc.new_file(sink, val_table.schema) as writer:
+            writer.write(val_table)
+
+    with open('train.lst', 'w', encoding="utf-8") as f:
+        f.write(train_path + '\n')
+
+    with open('val.lst', 'w', encoding="utf-8") as f:
+        f.write(val_path + '\n')
+
+    print(f"Done! Train: {len(train_ids)} rows, Val: {len(val_ids)} rows")
+
+
+if __name__ == "__main__":
+    main()
